efaaa3d2863f3fc76a34a3d2d6ccaf6dc2a0810d
AntonioRoccoGit <antonio.rocco97@gmail.com>
2023-03-24 15:29:14 +0100

    added snack 2

--- js/script.js
+++ js/script.js
@@ -1,13 +1,47 @@
+////////////////////////////////////////////////////////
+///////////////////////////SNAK 1///////////////////////
+
+// const bikes = [
+//     {
+//         name: "Carrera",
+//         weight: 10
+//     },
+//     {
+//         name: "Bmx",
+//         weight: 8
+//     },
+//     {
+//         name: "Mountain Bike",
+//         weight: 11
+//     },
+//     {
+//         name: "Downhill",
+//         weight: 6
+//     }
+// ];
+
+// //prelevo i pesi delle bici
+// const bikesWeight = bikes.map((curBike) => curBike.weight)
+// console.log(bikesWeight);
+
+// //prendo la bici con il peso piu basso
+// let minorWeight = Math.min(...bikesWeight);
+// console.log(minorWeight);
+
+// bikes.forEach((bike) => {
+//     if (bike.weight === minorWeight) {
+//         theChoosenOne = document.querySelector(".container");
+//         theChoosenOne.innerHTML = `La bici più leggera è una ${bike.name} e pesa ${bike.weight}Kg`
+//     }
+
+// });
+
+////////////////////////////////////////////////////////
+///////////////////////////SNAK 2///////////////////////
+
 /* 
-Snack 1
-Creare un array di oggetti:
-Ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e peso.
-Stampare a schermo la bici con peso minore utilizzando destructuring e template literal
 Snack2
-Creare un array di oggetti di squadre di calcio. Ogni squadra avrà diverse proprietà: nome, punti fatti, falli subiti.
-Nome sarà l’unica proprietà da compilare, le altre saranno tutte settate a 0.
-Generare numeri random al posto degli 0 nelle proprietà:
-Punti fatti e falli subiti.
+
 Infine usando la destrutturazione creiamo un nuovo array i cui elementi contengono solo nomi e falli subiti e stampiamo tutto in console.
 Snack Bonus
 Scrivere una funzione che accetti tre argomenti, un array e due numeri (a più piccolo di b).
@@ -15,49 +49,55 @@ La funzione ritornerà un nuovo array con i valori che hanno la posizione compre
 Usiamo i nuovi metodi degli array foreach o filter.
 */
 
-////////////////////////////////////////////////////////
-///////////////////////////SNAK 1///////////////////////
-
-const bikes = [
+const soccerTeams = [
     {
-        name: "Carrera",
-        weight: 10
+        name: "Napoli",
+        point: 0,
+        sufferdFoul: 0
     },
     {
-        name: "Bmx",
-        weight: 8
+        name: "Milan",
+        point: 0,
+        sufferdFoul: 0
     },
     {
-        name: "Mountain Bike",
-        weight: 11
+        name: "Inter",
+        point: 0,
+        sufferdFoul: 0
     },
     {
-        name: "Downhill",
-        weight: 6
-    }
+        name: "Juventus",
+        point: 0,
+        sufferdFoul: 0
+    },
+    {
+        name: "Sampdoria",
+        point: 0,
+        sufferdFoul: 0
+    },
 ];
 
-//prelevo i pesi delle bici
-const bikesWeight = bikes.map((curBike) => curBike.weight)
-console.log(bikesWeight);
-
-//prendo la bici con il peso piu basso
-let minorWeight = Math.min(...bikesWeight);
-console.log(minorWeight);
-
-bikes.forEach((bike) => {
-    if (bike.weight === minorWeight) {
-        theChoosenOne = document.querySelector(".container");
-        theChoosenOne.innerHTML = `La bici più leggera è una ${bike.name} e pesa ${bike.weight}Kg`
-    }
-
+soccerTeams.forEach((team) => {
+    team.point = getRndInteger(0, 90)
+    team.sufferdFoul = getRndInteger(0, 40)
 });
 
-/////////////////////////
-//FUNCTIONS 
+const teamNameAndFoul = [];
 
-function minorNumber(numb1, numb2, numb3){
-    let minor;
+soccerTeams.forEach((team) => {
+    const {name, sufferdFoul} = team;
     
+    const newObj = {name, sufferdFoul};
+    teamNameAndFoul.push(newObj);
+});
 
-};
+
+console.log(teamNameAndFoul, soccerTeams);
+
+
+//////////////////////////////////
+// FUNCTION
+
+function getRndInteger(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) ) + min;
+  }
