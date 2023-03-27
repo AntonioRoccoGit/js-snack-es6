6dee2b3458991646830bc4a401afc166b20ec1dc
AntonioRoccoGit <antonio.rocco97@gmail.com>
2023-03-27 10:18:19 +0200

    refactor code

--- js/script.js
+++ js/script.js
@@ -39,84 +39,79 @@
 ////////////////////////////////////////////////////////
 ///////////////////////////SNAK 2///////////////////////
 
-// const soccerTeams = [
-//     {
-//         name: "Napoli",
-//         point: 0,
-//         sufferdFoul: 0
-//     },
-//     {
-//         name: "Milan",
-//         point: 0,
-//         sufferdFoul: 0
-//     },
-//     {
-//         name: "Inter",
-//         point: 0,
-//         sufferdFoul: 0
-//     },
-//     {
-//         name: "Juventus",
-//         point: 0,
-//         sufferdFoul: 0
-//     },
-//     {
-//         name: "Sampdoria",
-//         point: 0,
-//         sufferdFoul: 0
-//     },
-// ];
+const soccerTeams = [
+    {
+        name: "Napoli",
+        point: 0,
+        sufferdFoul: 0
+    },
+    {
+        name: "Milan",
+        point: 0,
+        sufferdFoul: 0
+    },
+    {
+        name: "Inter",
+        point: 0,
+        sufferdFoul: 0
+    },
+    {
+        name: "Juventus",
+        point: 0,
+        sufferdFoul: 0
+    },
+    {
+        name: "Sampdoria",
+        point: 0,
+        sufferdFoul: 0
+    },
+];
 
-// soccerTeams.forEach((team) => {
-//     team.point = getRndInteger(0, 90)
-//     team.sufferdFoul = getRndInteger(0, 40)
-// });
+soccerTeams.forEach((team) => {
+    team.point = getRndInteger(0, 90)
+    team.sufferdFoul = getRndInteger(0, 40)
+});
 
-// const teamNameAndFoul = [];
+const teamNameAndFoul = [];
 
-// soccerTeams.forEach((team) => {
-//     const {name, sufferdFoul} = team;
-    
-//     const newObj = {name, sufferdFoul};
-//     teamNameAndFoul.push(newObj);
-// });
+soccerTeams.forEach(({name, sufferdFoul}) =>  teamNameAndFoul.push({name, sufferdFoul}));
 
 
-// console.log(teamNameAndFoul, soccerTeams);
+console.log(teamNameAndFoul, soccerTeams);
 
 
-// //////////////////////////////////
-// // FUNCTION
+//////////////////////////////////
+// FUNCTION
 
-// function getRndInteger(min, max) {
-//     return Math.floor(Math.random() * (max - min + 1) ) + min;
-//   }
+function getRndInteger(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) ) + min;
+  }
 
 ////////////////////////////////////////////////////////
 ///////////////////////////SNAK 3///////////////////////
 
-const numbersArray = [23, 14, 6, 8, 10];
+// const numbersArray = [23, 14, 6, 8, 10];
 
-const numbersArrayFunc = snackBonus2(numbersArray, 1, 3);
-console.log(numbersArrayFunc);
-//dato un array 
-//prendere gli indici compresi tra A e B 
-//stampare nuovo array con soli gli indici indicati
+// const numbersArrayFunc = snackBonus2(numbersArray, 1, 3);
+// console.log(numbersArrayFunc);
+// //dato un array 
+// //prendere gli indici compresi tra A e B 
+// //stampare nuovo array con soli gli indici indicati
 
-function snackBonus(array, a, b){
-    const newArray = [];
-    for(let i = a; i <= b; i++){
-        newArray.push(array[i]);
-    }
-    return newArray;
-};
-//implemented the same function with a different method
-function snackBonus2 (array, a, b){
-    const newArray = [];
-    array.forEach((numb, index) => {
-        if(index >= a && index <= b){
-            newArray.push(numb);
-        }
-    });
-    return newArray;
-};
+// function snackBonus(array, a, b){
+//     const newArray = [];
+//     for(let i = a; i <= b; i++){
+//         newArray.push(array[i]);
+//     }
+//     return newArray;
+// };
+// //implemented the same function with a different method
+// function snackBonus2 (array, a, b){
+//     const newArray = [];
+//     array.forEach((numb, index) => {
+//         if(index >= a && index <= b){
+//             newArray.push(numb);
+//         }
+//     });
+//     return newArray;
+// };
